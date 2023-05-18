b72a31c0d7e3acb19e832d5eb999748d3974cb0a
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-18 19:27:42 +0530

    commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,18 @@ import {logo} from "../../assets/index"
 
 const Header = () => {
   return (
-    <div className='w-full bg-amazon_blue text-white px-4 py-3'>
-        {/* ============ Image Start here =========== */}
+      <div>
+        <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+          {/* ============ Image Start here =========== */}
           <div className='px-2 h-[80%] flex items-center border border-transparent
            hover:border-white cursor cursor-pointer duration-100'>
             <img className= "w-24 mt-2" src={logo} alt="logo" />
           </div>
         {/* ============ Image End here =========== */}
         {/* ============ Deliver Start here =========== */}
+<div>
+  
+</div>
 
         {/* ============ Deliver Start here =========== */}
         {/* ============ Search Start here =========== */}
@@ -25,6 +29,7 @@ const Header = () => {
         {/* ============ Cart Start here =========== */}
 
         {/* ============ Cart Start here =========== */}
+        </div>
     </div>
   )
 }
