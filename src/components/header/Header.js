1b2a4d4c72fa99a34c52c7d01416dfdec967cb2b
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-18 16:56:10 +0530

    Commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
   return (
     <div className='w-full bg-amazon_blue text-white px-4 py-3'>
         {/* ============ Image Start here =========== */}
-          <div>
+          <div className='px-2 h-[80%] flex items-center border border-transparent
+           hover:border-white cursor cursor-pointer duration-100'>
             <img className= "w-24 mt-2" src={logo} alt="logo" />
           </div>
         {/* ============ Image End here =========== */}
