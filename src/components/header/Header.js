a3ffb9dc47f52ab027e18b726ee9fb17173d0ec1
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-18 16:33:06 +0530

    Commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import logo from"../../assets/logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+        {/* ============ Image Start here =========== */}
+
+        {/* ============ Image End here =========== */}
+        {/* ============ Deliver Start here =========== */}
+
+        {/* ============ Deliver Start here =========== */}
+        {/* ============ Search Start here =========== */}
+
+        {/* ============ Search Start here =========== */}
+        {/* ============ Signin Start here =========== */}
+        
+        {/* ============ Signin Start here =========== */}
+        {/* ============ Orders Start here =========== */}
+
+        {/* ============ Orders Start here =========== */}
+        {/* ============ Cart Start here =========== */}
+
+        {/* ============ Cart Start here =========== */}
+    </div>
   )
 }
 
