811a3bfa6a060749057814f12b97d87b6140eb52
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-20 19:31:21 +0530

    Commit

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -35,7 +35,12 @@ const HeaderBottom = () => {
                 Digital Content & Devices
               </h3>
               <ul>
-                <li>Amazon Music <span><KeyboardArrowRightIcon /></span></li>
+                <li className='flex items-center justify-between hover:bg zinc-200 px-6
+                py-2 cursor-pointer'>
+                  Amazon Music{" "}
+                  <span>
+                    <KeyboardArrowRightIcon />
+                    </span></li>
               </ul>
             </div>
           </div>
