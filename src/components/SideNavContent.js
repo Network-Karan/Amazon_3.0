7e4703106acdcea6e2585bcf883b32756ee869a0
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-20 22:05:51 +0530

    Commit

--- src/components/SideNavContent.js
+++ src/components/SideNavContent.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const SideNavContent = () => {
   return (
-    <div>SideNavContent</div>
+    <div>
+      <h3 className='text-lg font-titleFont font-semibold mb-1 px-6'>
+        Digital Content & Devices
+      </h3>
+      <ul>
+          <li className='flex items-center justify-between hover:bg-zinc-200 px-6
+            py-2 cursor-pointer'>
+            Amazon Music{" "}
+              <span>
+                <KeyboardArrowRightIcon />
+              </span>
+          </li>
+          <li className='flex items-center justify-between hover:bg-zinc-200 px-6
+            py-2 cursor-pointer'>
+            Amazon Music{" "}
+              <span>
+                <KeyboardArrowRightIcon />
+              </span>
+          </li>
+          <li className='flex items-center justify-between hover:bg-zinc-200 px-6
+            py-2 cursor-pointer'>
+            Amazon Music{" "}
+              <span>
+                <KeyboardArrowRightIcon />
+              </span>
+          </li>
+        </ul>
+    </div>
   )
 }
 
