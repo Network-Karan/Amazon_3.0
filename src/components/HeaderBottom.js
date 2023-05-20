841f54cf91a3d0f5c00f41add361c542e84be40d
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-20 21:44:50 +0530

    Commit

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -42,6 +42,20 @@ const HeaderBottom = () => {
                     <KeyboardArrowRightIcon />
                     </span>
                 </li>
+                <li className='flex items-center justify-between hover:bg-zinc-200 px-6
+                py-2 cursor-pointer'>
+                  Amazon Music{" "}
+                  <span>
+                    <KeyboardArrowRightIcon />
+                    </span>
+                </li>
+                <li className='flex items-center justify-between hover:bg-zinc-200 px-6
+                py-2 cursor-pointer'>
+                  Amazon Music{" "}
+                  <span>
+                    <KeyboardArrowRightIcon />
+                    </span>
+                </li>
               </ul>
             </div>
           </div>
