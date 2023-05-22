fbb5f1fcc7954ddc35fe30bdbc02fbe86f6c0070
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-22 11:37:19 +0530

    Commit

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import Slider from "react-slick";
+import {
+    bannerImgOne,
+    bannerImgTwo,
+    bannerImgThree,
+    bannerImgFour,
+    bannerImgFive,
+} from '../../assets';
 
 const Banner = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
   return (
-    <div>Banner</div>
+    <div>
+        
+        <Slider {...settings}>
+          <div>
+            <img src={bannerImgOne} alt='bannerImgOne' />
+          </div>
+          <div>
+            <img src={bannerImgTwo} alt='bannerImgTwo' />
+          </div>
+          <div>
+            <img src={bannerImgThree} alt='bannerImgThree' />
+          </div>
+          <div>
+            <img src={bannerImgFour} alt='bannerImgFour' />
+          </div>
+          <div>
+            <img src={bannerImgFive} alt='bannerImgFive' />
+          </div>
+        </Slider>
+      </div>
   )
 }
 
