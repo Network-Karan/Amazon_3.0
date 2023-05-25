59275035b8cda34e9b4f13a23d4b45d9530922b7
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-25 12:37:17 +0530

    Commit

--- src/components/footer/FooterMiddleList.js
+++ src/components/footer/FooterMiddleList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const FooterMiddleList = ({title,listItem}) => {
-    console.log(listItem)
   return (
     <div>
         <h3 className='font-titleFont text-white text-base fontsemibold mb-3'>
@@ -9,7 +8,7 @@ const FooterMiddleList = ({title,listItem}) => {
         </h3>
         <ul className='flex flex-col gap-2 font-bodyFont'>
           {listItem.map((item) =>
-            item.listData.map((data) => <li className='footerLink'>{data}</li>)
+            item.listData.map((data,i) => <li key={i} className='footerLink'>{data}</li>)
           )}
         </ul>
     </div>
