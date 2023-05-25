7c915806689b767f01a1c11ba4b1b0ec98900fc8
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-25 12:34:52 +0530

    Commit

--- src/components/footer/FooterMiddle.js
+++ src/components/footer/FooterMiddle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FooterMiddleList from './FooterMiddleList'
+import { middleList } from '../../constants'
 
 const FooterMiddle = () => {
   return (
@@ -8,10 +9,13 @@ const FooterMiddle = () => {
       <div className='w-full border-b-[1px] border-gray-500 py-10'>
         <div className='max-w-5xl mx-auto text-gray-300'>
           <div className='w-full grid grid-cols-4 place-items-center items-start'>
-            <FooterMiddleList />
-            <FooterMiddleList />
-            <FooterMiddleList />
-            <FooterMiddleList />
+            {middleList.map((item)=>(
+              <FooterMiddleList 
+                key={item._id}  
+                title={item.title}
+                listItem={item.listItem}
+                />
+              ))}
           </div>
         </div>
       </div>
