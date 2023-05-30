4a0281e2e5882ca2d7b9c21353fb77177273e2b6
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2023-05-30 20:34:04 +0530

    Commit

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -11,55 +11,64 @@ const Products = () => {
     const productsData = data.data;
     console.log(productsData)
   return (
-    <div className='max-w-screen-2xl mx-auto grid grid-cols-4 gap-10 px-4'>
-      {productsData.map((items)=>(
-          <div key={items.id} className='bg-white h-auto border-[1px] border-gray-200 py-8 z-30
-          hover:border-transparent shadow-none hover:shadow-testShadow duration-200
-          flex flex-col gap-4 relative'>
-            <div className='w-full h-auto flex items-center justify-center relative group'>
-            <img 
-              className='w-52 h-64 object-contain' 
-              src={items.image} 
-              alt="ProductImg" 
-            />
-            <ul className='w-full h-36 bg-gray-100 absolute bottom-[-170px] flex flex-col items-end
-            justify-center gap-2 font-titleFont px-2 border-l border-r group-hover:bottom-0 duration-700'>
-              <li className='productLi'>
-                Compare{" "}
-                <span>
-                  <ApiIcon />
-                </span>
-              </li>
-              <li className='productLi'>
-                Add to Cart{" "}
-                <span>
-                  <ShoppingCartIcon />
-                </span>
-              </li>
-              <li className='productLi'>
-                View Details{" "}
-                <span>
-                  <ArrowCircleRightIcon />
-                </span>
-              </li>
-              <li className='productLi'>
-                Add to Wish Lists{" "}
-                <span>
-                  <FavouriteIcon />
-                </span>
-              </li>
-            </ul>
-              <span className='text-xs capitalize italic absolute top-2 right-2 text-gray-500'>{items.category}</span>
-            </div>
-            <div className='px-4 z-10 bg-white'>
-            <div className='flex items-center justify-between'>
-              <h2 className='font-titleFont tracking-wide text-lg text-amazon_blue
-              font-medium'>{items.title.substring(0, 20)}</h2>
-              <p className='text-sm text-gray-600 font-semibold'>
-                ${items.price}</p>
-              </div>
+    <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 xl:gap-4 px-4">
+    {productsData.map((item) => (
+      <div
+        key={item.id}
+        className="bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent shadow-none hover:shadow-testShadow duration-200 relative flex flex-col gap-4"
+      >
+        <span className="text-xs capitalize italic absolute top-2 right-2 text-gray-500">
+          {item.category}
+        </span>
+        {/* ========== Product Image Start here ============== */}
+        <div className="w-full h-auto flex items-center justify-center relative group">
+          <img
+            className="w-52 h-64 object-contain"
+            src={item.image}
+            alt="ProductImg"
+          />
+          {/* ================== Product mini drop down Start here ============ */}
+          <ul className="absolute w-full h-36 bg-gray-100 -bottom-[160px] group-hover:bottom-0 duration-700 flex flex-col justify-center items-end gap-2">
+            <li className="productLi">
+              Compare
+              <span>
+                <ApiIcon />
+              </span>
+            </li>
+            <li className="productLi">
+              Add to Cart
+              <span>
+                <ShoppingCartIcon />
+              </span>
+            </li>
+            <li className="productLi">
+              View Details{" "}
+              <span>
+                <ArrowCircleRightIcon />
+              </span>
+            </li>
+            <li className="productLi">
+              Add to Wish List{" "}
+              <span>
+                <FavouriteIcon />
+              </span>
+            </li>
+          </ul>
+          {/* ================== Product mini drop down End here ============== */}
+        </div>
+        {/* ========== Product Image End here ================ */}
+        {/* ========== Product Info Start here =============== */}
+        <div className="px-4 bg-white flex flex-col gap-1 z-10">
+          <div className="flex items-center justify-between">
+            <h2 className="font-titleFont tracking-wide text-lg text-amazon_blue font-medium">
+              {item.title.substring(0, 20)}
+            </h2>
+            <p className="text-sm text-gray-600 font-semibold">
+              ${item.price}
+            </p>
+          </div>
               <div>
-                <p className='text-sm'>{items.description.substring(0, 100)}...</p>
+              <p className="text-sm">{item.description.substring(0, 100)}</p>
                 <div className='text-yellow-500'>
                   <StarIcon />
                   <StarIcon />
